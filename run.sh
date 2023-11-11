059bc09cee535873a7a82a8f49f4b1cdecc50208
metadings <metadings@live.de>
2023-11-11 20:59:07 +0100

    MINCLR: Introducing test.sh

--- run.sh
+++ run.sh
@@ -10,24 +10,4 @@ run.awk ~/FIKTIV/make.awk/make.awk -std +shared +debug project=System precompile
 # Extension
 run.awk ~/FIKTIV/make.awk/make.awk -std +shared +debug project=Extension precompile include/ .Extension.c compile library 1>&2 2>.Extension...report
 
-
-cd .make
-
-INTERP=
-#INTERP=./System.Interpreter
-REPORT=System.test...report
-
-$INTERP ./ELFAssembly.read Crypto.SipHash.test > ELFAssembly.read...report 2>&1
-echo -n > $REPORT
-$INTERP ./Crypto.SipHash.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.Exception.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.File.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.FileInfo.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.Math.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.Memory.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.Path.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.Signal.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.Syscall.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.Thread.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.string8.test >> $REPORT 2>&1 ; echo >> $REPORT
-$INTERP ./System.values.test >> $REPORT 2>&1 ; echo >> $REPORT
+./test.sh
---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ PWD != '${HOME}/FIKTIV/MINCLR' ]; then
+    cd $HOME/FIKTIV/MINCLR
+fi
+
+cd .make
+
+INTERP=
+#INTERP=./System.Interpreter
+REPORT=System.test...report
+
+$INTERP ./ELFAssembly.read Crypto.SipHash.test > ELFAssembly.read...report 2>&1
+echo -n > $REPORT
+$INTERP ./Crypto.SipHash.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.Exception.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.File.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.FileInfo.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.Math.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.Memory.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.Path.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.Signal.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.Syscall.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.Thread.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.string8.test >> $REPORT 2>&1 ; echo >> $REPORT
+$INTERP ./System.values.test >> $REPORT 2>&1 ; echo >> $REPORT
