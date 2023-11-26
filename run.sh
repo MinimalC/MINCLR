b7e1de8f7397eb676b44ac36a345e3b5187b6889
metadings <metadings@live.de>
2023-11-26 12:45:11 +0100

    MINCLR: Extension.c

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ compile library executable  source/main/System.Runtime.shared.c source/main/Syst
 source/main/WebService1.c source/main/test/Crypto.SipHash.test.c source/main/test/System.Exception.test.c source/main/test/System.File.test.c source/main/test/System.FileInfo.test.c source/main/test/System.Math.test.c source/main/test/System.Memory.test.c source/main/test/System.Path.test.c source/main/test/System.Signal.test.c source/main/test/System.Syscall.test.c source/main/test/System.Thread.test.c source/main/test/System.string8.test.c source/main/test/System.values.test.c source/main/Eratosthenes.c source/main/ELFAssembly.read.c 1>&2 2>.System...report
 
 # Extension
-run.awk ~/FIKTIV/make.awk/make.awk -std +shared +debug project=Extension precompile include/ .Extension.c compile library 1>&2 2>.Extension...report
+run.awk ~/FIKTIV/make.awk/make.awk -std +shared +debug project=Extension precompile include/ source/main/Extension.c compile library 1>&2 2>.Extension...report
 
 ./test.sh
---
+++ source/main/Extension.c
@@ -0,0 +1,8 @@
+#define using_System
+#include <min/System.h>
+
+
+void Say_Hello() {
+
+    Console_writeLine__string("Hello World!");
+}
